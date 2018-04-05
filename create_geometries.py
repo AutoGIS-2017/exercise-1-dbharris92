8f3236df21f7b29deecccd72627ba6092d5330e3
dbharris92 <harris.db92@gmail.com>
2018-04-05 16:54:42 -0400

    solved first three

--- create_geometries.py
+++ create_geometries.py
@@ -6,24 +6,41 @@ Auto-GIS Exercise 1: Working With Geometries
 """
 
 # import shapely packages
-import shapely
+from shapely.geometry import Point, MultiPoint, LineString, Polygon
+
 
 # function to create a shapely point geometry object from x, y coords
 def createPointGeom(x_coord, y_coord):
     return Point(x_coord, y_coord)
 
-# Test points for below function
-points = [Point(2.2, 4.2), Point(7.2, -25.1), Point(9.26, -2.456)]
-
 # function to create shapely line object from shapley input points list
-def createLineGeom(giveLine):
-    if type(giveLine) is shapely.geometry.point.Point:
-        return LineString(giveLine)
+# sample vertices 
+vertices = [Point(2.2, 4.2), Point(7.2, -25.1), Point(9.26, -2.456)]
+
+# function 
+def createLineGeom(inList):
+    if all(isinstance(x, Point) for x in inList) is True:
+       return LineString(inList)
     else:
         return 'pointList must contain shapely point object(s)'
+my_Line = createLineGeom(vertices)
+print(my_Line)
 
-pls_work = createLineGeom(points)
+# function to take list of coordinate tuples or shapely points and return 
+# input points and coords
+point1 = Point(2.2, 4.2)
+point2 = Point(7.2, -25.1)
+point3 = Point(9.26, -2.456)
+
+point_coords = ((point1.coords), (point2.coords), (point3.coords)) 
+
+# function (partially solved)
+def createPolyGeom(givePoly):
+    for x in givePoly:
+        return Polygon([[p.x, p.y] for p in givePoly])
+    else:
+        return 'givePoly must contain point objects or point tuples'
 
 
 
-
+ 
